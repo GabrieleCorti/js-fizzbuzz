3c2e8f3842f32477d96a2e1aea88f98a52c58f86
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-02 14:39:36 +0100

    la stampa viene compilata correttamente

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,19 @@
-alert("lmao");
+for (var i = 1; i <= 100; i++) {
+
+  if (i % 3 == 0 && i % 5 == 0 ) {
+
+    console.log("Fizzbuzz");
+
+  } else if (i % 3 == 0 && i % 5 != 0) {
+
+      console.log("Fizz");
+
+  } else if (i % 3 != 0 && i % 5 == 0){
+
+      console.log("Buzz");
+
+  } else {
+    console.log(i)
+  }
+
+}
